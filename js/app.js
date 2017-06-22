7f39e0336fa94a0e63454c3d256698ed4a7a013d
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-21 22:02:54 -0400

    commenting out the first in the second

--- js/app.js
+++ js/app.js
@@ -50,15 +50,15 @@ angular
   }
 
  function WdinstagramIndexControllerFunction(){
-   this.wdinstagrams = [
-     {photo_url: "http://i.imgur.com/RpcP2Ax.jpg", author: "Oscar Wilde", body: "The Picture of Dorian Gray: Written in his distinctively dazzling manner, Oscar Wilde’s story of a fashionable young man who sells his soul for eternal youth and beauty is the author’s most popular work. The tale of Dorian Gray’s moral disintegration caused a scandal when it ﬁrst appeared in 1890, but though Wilde was attacked for the novel’s corrupting inﬂuence, he responded that there is, in fact, “a terrible moral in Dorian Gray.” Just a few years later, the book and the aesthetic/moral dilemma it presented became issues in the trials occasioned by Wilde’s homosexual liaisons, which resulted in his imprisonment. Of Dorian Gray’s relationship to autobiography, Wilde noted in a letter, “Basil Hallward is what I think I am: Lord Henry what the world thinks me: Dorian what I would like to be—in other ages, perhaps."},
-     {photo_url: "http://i.imgur.com/FSrKQUC.jpg", author: "Fyodor Dostoevsky", body: "Through the story of the brilliant but conflicted young Raskolnikov and the murder he commits, Fyodor Dostoevsky explores the theme of redemption through suffering. Crime and Punishment put Dostoevsky at the forefront of Russian writers when it appeared in 1866 and is now one of the most famous and influential novels in world literature."}
-    ]
-
-   this.create = function(){
-     var data = {photo_url: this.photo_url, author: this.author, body: this.body};
-     this.wdinstagrams.push(data);
-   }
+  //  this.wdinstagrams = [
+  //    {photo_url: "http://i.imgur.com/RpcP2Ax.jpg", author: "Oscar Wilde", body: "The Picture of Dorian Gray: Written in his distinctively dazzling manner, Oscar Wilde’s story of a fashionable young man who sells his soul for eternal youth and beauty is the author’s most popular work. The tale of Dorian Gray’s moral disintegration caused a scandal when it ﬁrst appeared in 1890, but though Wilde was attacked for the novel’s corrupting inﬂuence, he responded that there is, in fact, “a terrible moral in Dorian Gray.” Just a few years later, the book and the aesthetic/moral dilemma it presented became issues in the trials occasioned by Wilde’s homosexual liaisons, which resulted in his imprisonment. Of Dorian Gray’s relationship to autobiography, Wilde noted in a letter, “Basil Hallward is what I think I am: Lord Henry what the world thinks me: Dorian what I would like to be—in other ages, perhaps."},
+  //    {photo_url: "http://i.imgur.com/FSrKQUC.jpg", author: "Fyodor Dostoevsky", body: "Through the story of the brilliant but conflicted young Raskolnikov and the murder he commits, Fyodor Dostoevsky explores the theme of redemption through suffering. Crime and Punishment put Dostoevsky at the forefront of Russian writers when it appeared in 1866 and is now one of the most famous and influential novels in world literature."}
+  //   ]
+   //
+  //  this.create = function(){
+  //    var data = {photo_url: this.photo_url, author: this.author, body: this.body};
+  //    this.wdinstagrams.push(data);
+  //  }
  };
 
  function WdinstagramShowControllerFunction(){
